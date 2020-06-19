499f1fc357a160c318fb69a8095d344617652c3c
Snowmagedden <66143690+Snowmagedden@users.noreply.github.com>
2020-06-19 13:44:09 +0200

    Add files via upload

---
+++ Conv2D/Extracting images from videos.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+import os
+import pandas as pd
+from tqdm import tqdm
+import cv2
+
+
+# In[ ]:
+
+
+#Creating a list of videos
+list_of_files = []
+for filename in os.listdir('videos'):
+    list_of_files.append(filename)
+
+#Creating a dataframe out of the list
+df = pd.DataFrame(list_of_files)
+df.columns = ['video name']
+
+
+# In[ ]:
+
+
+# @Kang&Atul
+for i in tqdm(range(df.shape[0])):
+    count = 0
+    videoFile = df['video name'][i]
+    # Opens the Video file
+    cap= cv2.VideoCapture('videos/'+ videoFile) #videos folder
+    i=0   # setting this to 0 gives first frame but not last// setting to 1 gives last frame but not first
+    while(cap.isOpened()):
+        ret, frame = cap.read()
+        if ret == False:
+            break
+        if i%125 == 0:      # total of 3000 frames per image   (i = 3000 %125 is divided by 125 so this gives 24 frames)
+            filename ='image frames/'+ videoFile + "_frame%d.jpg" % count;count+=1  #saves into image frames folder
+            cv2.imwrite(filename, frame)
+        i+=1
+ 
+    cap.release()
+    cv2.destroyAllWindows()
+
---
+++ Conv2D/Running the model.py
@@ -0,0 +1,151 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[6]:
+
+
+import numpy as np
+from keras.preprocessing import image
+from tqdm import tqdm
+from keras.callbacks import ModelCheckpoint
+from keras.applications.resnet50 import ResNet50
+import matplotlib.pyplot as plt
+from keras.models import Sequential
+from keras.layers import Dense, InputLayer, Dropout, Flatten
+from keras.layers import Conv2D, MaxPooling2D, GlobalMaxPooling2D
+import pandas as pd
+
+
+# In[7]:
+
+
+final = pd.read_csv('final.csv')
+
+
+# In[9]:
+
+
+# creating an empty list of image arrays of the image frames
+image_array = []
+
+# for loop to read and store frames
+for i in tqdm(range(final.shape[0])):
+    # loading the image and keeping the target size as (224,224,3)
+    img = image.load_img('image frames/'+final['path'][i], target_size=(224,224,3))    # also try 250 x 250
+    # converting it to array
+    img = image.img_to_array(img)
+    # normalizing the pixel value
+    img = img/255                                    
+    # appending the image to the train_image list
+    image_array.append(img)
+
+
+# In[12]:
+
+
+# converting the list to numpy array and splitting into X_train and X_test
+X_train = np.array(image_array[0:6576])
+X_test = np.array(image_array[6576:8880])
+
+
+# In[15]:
+
+
+y = final['engagement rating']
+y = y.astype(int)
+y = np.array(y)
+y-= 1                     #We need -1 because python starts at 0
+
+
+# In[16]:
+
+
+#splitting the y_train and y_test
+y_train = y[0:6576]
+y_test = y[6576:8880]
+
+
+# In[17]:
+
+
+#Assigning ResNet50 to a variable 
+base_model = ResNet50(weights='imagenet', include_top=False, input_shape=(224, 224, 3))
+
+
+# In[ ]:
+
+
+#Extracting features from the images using ResNet
+#This is very computationally demanding and will take a long time
+X_train = base_model.predict(X_train)
+X_test = base_model.predict(X_test)
+
+
+# In[ ]:
+
+
+# normalizing the pixel values
+max = X_train.max()
+X_train = X_train/max
+X_test = X_test/max
+
+
+# In[ ]:
+
+
+#Building the model
+def build_model():
+    model = tf.keras.models.Sequential()
+    model.add(tf.keras.layers.Conv2D(filters=128, kernel_size=3, strides=1, padding="same", activation=None,
+                             input_shape=[7,7,2048]))
+    model.add(tf.keras.layers.Conv2D(filters=128, kernel_size=3, strides=1, padding="same", activation=None))
+    model.add(tf.keras.layers.BatchNormalization())
+    model.add(tf.keras.layers.MaxPool2D(pool_size=1, strides=2))
+    model.add(tf.keras.layers.Activation('relu'))
+    model.add(tf.keras.layers.Conv2D(filters=128, kernel_size=3, strides=1, padding="same", activation=None))
+    model.add(tf.keras.layers.BatchNormalization())
+    model.add(tf.keras.layers.MaxPool2D(pool_size=1, strides=2))
+    model.add(tf.keras.layers.Activation('relu'))
+    model.add(tf.keras.layers.Conv2D(filters=128, kernel_size=3, strides=1, padding="same", activation=None))
+    model.add(tf.keras.layers.BatchNormalization())
+    model.add(tf.keras.layers.MaxPool2D(pool_size=1, strides=2))
+    model.add(tf.keras.layers.Activation('relu'))
+    model.add(tf.keras.layers.Conv2D(filters=128, kernel_size=3, strides=1, padding="same", activation=None))
+    model.add(tf.keras.layers.BatchNormalization())
+    model.add(tf.keras.layers.MaxPool2D(pool_size=1, strides=2))
+    model.add(tf.keras.layers.Activation('relu'))
+    model.add(tf.keras.layers.Flatten())
+    model.add(tf.keras.layers.Dense(5, activation='softmax'))
+    return model
+
+
+# In[ ]:
+
+
+model = build_model()
+mcp_save = ModelCheckpoint('weight.hdf5', save_best_only=True, monitor='val_loss', mode='min')
+model.compile(loss='sparse_categorical_crossentropy',optimizer=tf.keras.optimizers.Adadelta(lr=0.002),metrics=['accuracy'])
+
+
+# In[ ]:
+
+
+history = model.fit(X_train, y_train, epochs=10, validation_data=(X_test, y_test), batch_size=128)
+
+
+# In[ ]:
+
+
+history_dict = history.history
+acc = history_dict['accuracy']
+val_acc = history_dict['val_accuracy']
+loss_values = history_dict['loss']
+epochs = range(1, len(loss_values) + 1)
+plt.plot(epochs, acc, 'r', label='Training acc') 
+plt.plot(epochs, val_acc, 'b', label='Test acc') 
+plt.title('Training and test accuracy') 
+plt.xlabel('Epochs')
+plt.ylabel('Accuracy') 
+plt.legend()
+plt.show()
+
